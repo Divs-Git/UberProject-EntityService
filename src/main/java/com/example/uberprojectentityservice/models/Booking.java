9a813eeb52de03bad704ab951bdf9ca587005473
Divs-Git <divyansh.sri258@gmail.com>
2025-07-16 19:26:22 +0530

    added common entity service

---
+++ src/main/java/com/example/uberprojectentityservice/models/Booking.java
@@ -0,0 +1,35 @@
+package com.example.uberprojectentityservice.models;
+import com.example.uberprojectentityservice.models.BaseModel;
+import com.example.uberprojectentityservice.models.BookingStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Booking extends BaseModel {
+
+    @Enumerated(value = EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private Long totalDistance;
+
+    @ManyToOne
+    private Driver driver;
+
+    @ManyToOne
+    private Passenger passenger;
+
+}
